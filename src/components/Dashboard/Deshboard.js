fa9877f62bffd9b9199d1aa6132baf71ece46ef9
Murshedul <murshedbinamir@gmail.com>
2022-04-04 17:59:46 +0600

    product analysis site initialized and route components created

---
+++ src/components/Dashboard/Deshboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dashboard.css';
+
+const Deshboard = () => {
+    return (
+        <div>
+            <h2>This is Dashboard page</h2>
+        </div>
+    );
+};
+
+export default Deshboard;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='mt-16'>
+            <span className='text-6xl font-bold'>404</span><br /><br />
+            <span className='text-4xl font-bold'>Sorry, Page Not Found</span>
+        </div>
+    );
+};
+
+export default NotFound;
