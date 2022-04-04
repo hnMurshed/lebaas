597ffc3ea17a3c8203f10a4b141e2bbeefc01d4c
Murshedul <murshedbinamir@gmail.com>
2022-04-04 19:38:58 +0600

    navigation created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='text-5xl'>Header</h2>
+        <div className='bg-[#0080ff] text-white py-6'>
+            <div className="container flex justify-between items-center">
+                <Link to='/'><span className='text-3xl font-bold cursor-pointer'>LEBAAS</span></Link>
+                <ul className='flex gap-7'>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/reviews'>Reviews</Link>
+                    </li>
+                    <li>
+                        <Link to='/dashboard'>Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to='/blogs'>Blogs</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
